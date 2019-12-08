291ec86fdc7a9d24d2edfae34aa9e97746d72b0d
Penguin <ad0097@uah.edu>
2019-12-07 23:09:08 -0600

    added tm4c123x example project. wont compile for some reason

---
+++ embedded/stm32discovery-tinkering/src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+// pick a panicking behavior
+extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// extern crate panic_abort; // requires nightly
+// extern crate panic_itm; // logs messages over ITM; requires ITM support
+// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+
+use cortex_m::peripheral::{syst, Peripherals};
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let mut systick = peripherals.SYST;
+    systick.set_clock_source(syst::SystClkSource::Core);
+    systick.set_reload(1_000);
+    systick.clear_current();
+    systick.enable_counter();
+    while !systick.has_wrapped()
+    {
+        // loop
+    }
+
+    loop {}
+}
